b4104defdf7f7dc2082763947356859980fbb408
Lê Văn Tùng <levantung2552@gmail.com>
2024-02-20 10:48:46 +0700

    update dialog

--- index.js
+++ index.js
@@ -5,10 +5,15 @@ for (let index = 1; index <= 15; index++) {
         <div id="img-${index}" class="thumbnail">
             <img id="thumbnail-${index}"  src="public/image/t-${index}.webp" alt="" onclick="playVideo(${index})">
         </div>
-        <div id="video-${index}" class="video" style="display: none;">
-            <video controls id="videoPlayer-${index}">
-                <source src="public/video/v-${index}.mp4" type="video/mp4">
-            </video>
+        <div id="video-${index}" class="video" style="display: none">
+            <div style="position: relative;">
+                <img id="close-${index}" src="public/x_close.png" class="close" onclick="closeVideo(${index})"/>
+
+                    <video controls id="videoPlayer-${index}">
+                        <source src="public/video/v-${index}.mp4" type="video/mp4">
+                    </video>
+
+            </div>
         </div>
     </div>
     `;
@@ -17,19 +22,27 @@ for (let index = 1; index <= 15; index++) {
 document.getElementById('wrapper').innerHTML = content;
 
 const playVideo = (index) => {
-    const thumbnail = document.getElementById(`thumbnail-${index}`);
     const videoPlayer = document.getElementById(`videoPlayer-${index}`);
     const videoItem = document.getElementById(`video-${index}`);
-    const imgItem = document.getElementById(`img-${index}`);
+    const layoutHidden = document.getElementById(`layout-hidden`);
 
 
-    imgItem.style.display = 'none';
     videoItem.style.display = 'block';
+    layoutHidden.style.display = 'block'
     videoPlayer.play();
 
     // Xử lý sự kiện kết thúc video
     videoPlayer.addEventListener('ended', () => {
-        imgItem.style.display = 'block';
         videoItem.style.display = 'none';
+        layoutHidden.style.display = 'none';
+        console.log(1);
     });
 };
+const closeVideo = (index)=>{
+    const videoItem = document.getElementById(`video-${index}`);
+    const videoPlayer = document.getElementById(`videoPlayer-${index}`);
+    const layoutHidden = document.getElementById(`layout-hidden`);
+    videoItem.style.display = 'none';
+    layoutHidden.style.display = 'none';
+    videoPlayer.close();
+}
